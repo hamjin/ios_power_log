94e887c8ac9743d46c53988497ab260582989cbc
hamjin <jinham@qq.com>
2023-06-04 18:39:48 +0800

    Fix cycles

--- inject.c
+++ inject.c
@@ -24,6 +24,8 @@ void start_routine(void *arg) {
 	pid_t pid = getpid();
 	uint64_t last = 0;
 	uint64_t last_p = 0;
+	uint64_t last_cycles = 0;
+	uint64_t last_pcycles = 0;
 	uint64_t last_mach = 0;	
 	struct timespec tim, tim2;
    	tim.tv_sec = 0;
@@ -36,10 +38,12 @@ void start_routine(void *arg) {
 
 		syslog(LOG_WARNING, "inject.c: power = %fw\n", ((usage.ri_energy_nj - last) / 1e9) * (1 / mach_time_to_seconds(delta)));
 		syslog(LOG_WARNING, "inject.c: ppower = %fw\n", ((usage.ri_penergy_nj - last_p) / 1e9) * (1 / mach_time_to_seconds(delta)));
-		syslog(LOG_WARNING, "inject.c: pcycles = %llu\n", usage.ri_pcycles);
-		syslog(LOG_WARNING, "inject.c: cycles = %llu\n", usage.ri_cycles);
+		syslog(LOG_WARNING, "inject.c: pcycles = %fw\n", (usage.ri_pcycles - last_pcycles) * (1 / mach_time_to_seconds(delta));
+		syslog(LOG_WARNING, "inject.c: cycles = %fw\n", (usage.ri_cycles - last_cycles) * (1 / mach_time_to_seconds(delta));
 		last = usage.ri_energy_nj;
 		last_p = usage.ri_penergy_nj;
+		last_pcycles = usage.ri_pcycles;
+		last_cycles = usage.ri_cycles;
 		last_mach = delta + last_mach;
 		nanosleep(&tim , &tim2);
 	}
